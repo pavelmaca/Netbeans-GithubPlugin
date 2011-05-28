3496a94215fb713de9af7cd06cc9f580b9bd583d
Pavel Máca <maca.pavel@gmail.com>
2011-05-28 15:22:00 +0200

    init option panel

---
+++ src/cz/inseo/netbeans/github/GithubOptionsPanelController.java
@@ -0,0 +1,72 @@
+package cz.inseo.netbeans.github;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import javax.swing.JComponent;
+import org.netbeans.spi.options.OptionsPanelController;
+import org.openide.util.HelpCtx;
+import org.openide.util.Lookup;
+
+@OptionsPanelController.SubRegistration(location = "Advanced",
+displayName = "#AdvancedOption_DisplayName_Github",
+keywords = "#AdvancedOption_Keywords_Github",
+keywordsCategory = "Advanced/Github")
+public final class GithubOptionsPanelController extends OptionsPanelController {
+
+	private GithubPanel panel;
+	private final PropertyChangeSupport pcs = new PropertyChangeSupport(this);
+	private boolean changed;
+
+	public void update() {
+		getPanel().load();
+		changed = false;
+	}
+
+	public void applyChanges() {
+		getPanel().store();
+		changed = false;
+	}
+
+	public void cancel() {
+		// need not do anything special, if no changes have been persisted yet
+	}
+
+	public boolean isValid() {
+		return getPanel().valid();
+	}
+
+	public boolean isChanged() {
+		return changed;
+	}
+
+	public HelpCtx getHelpCtx() {
+		return null; // new HelpCtx("...ID") if you have a help set
+	}
+
+	public JComponent getComponent(Lookup masterLookup) {
+		return getPanel();
+	}
+
+	public void addPropertyChangeListener(PropertyChangeListener l) {
+		pcs.addPropertyChangeListener(l);
+	}
+
+	public void removePropertyChangeListener(PropertyChangeListener l) {
+		pcs.removePropertyChangeListener(l);
+	}
+
+	private GithubPanel getPanel() {
+		if (panel == null) {
+			panel = new GithubPanel(this);
+		}
+		return panel;
+	}
+
+	void changed() {
+		if (!changed) {
+			changed = true;
+			pcs.firePropertyChange(OptionsPanelController.PROP_CHANGED, false, true);
+		}
+		pcs.firePropertyChange(OptionsPanelController.PROP_VALID, null, null);
+	}
+}
